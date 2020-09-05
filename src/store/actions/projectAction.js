bd0048f7e2b9d9f46b0c4a58b9fe39065a8bd52d
hendra-agil <agilsyaputra26@gmail.com>
2020-09-05 19:01:38 +0700

    Adding data to Firestore

--- src/store/actions/projectAction.js
+++ src/store/actions/projectAction.js
@@ -1,6 +1,21 @@
 export const createProject = (project) => {
   return (dispatch, getState, { getFirestore, getFirebase }) => {
     // Make async call to database
-    dispatch({ type: 'CREATE_PROJECT', project });
+    const firestore = getFirestore();
+    firestore
+      .collection('projects')
+      .add({
+        ...project,
+        authorFirstName: 'Agil',
+        authorLastName: 'Syaputra',
+        authorId: 12345,
+        createdAt: new Date(),
+      })
+      .then(() => {
+        dispatch({ type: 'CREATE_PROJECT', project });
+      })
+      .catch((err) => {
+        dispatch({ type: 'CREATE_PROJECT_ERROR', err });
+      });
   };
 };
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -10,11 +10,13 @@ const projectReducer = (state = initState, action) => {
   switch (action.type) {
     case 'CREATE_PROJECT':
       console.log('created project', action.project);
-      break;
+      return state;
+    case 'CREATE_PROJECT_ERROR':
+      console.log('create project error', action.err);
+      return state;
     default:
-      break;
+      return state;
   }
-  return state;
 };
 
 export default projectReducer;
