3581f23fcb2e7886cd0d13a7d5dd83515db2f617
hendra-agil <agilsyaputra26@gmail.com>
2020-09-05 17:02:38 +0700

    Adding redux & reducers

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './store/reducers/rootReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { combineReducers } from 'redux';
+import authReducer from './authReducer';
+import projectReducer from './projectReducer';
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  project: projectReducer,
+});
+
+export default rootReducer;
