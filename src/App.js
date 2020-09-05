c312c747b394e98ab08c1700a9220c8a9328cd09
hendra-agil <agilsyaputra26@gmail.com>
2020-09-05 14:40:34 +0700

    Dashboard component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
+        <Switch>
+          <Route path="/" component={Dashboard} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const ProjectList = () => {
+  return (
+    <div className="project-list section">
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text-text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Hendra Agil</p>
+          <p className="grey-text">28 August, 4pm</p>
+        </div>
+      </div>
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text-text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Hendra Agil</p>
+          <p className="grey-text">28 August, 4pm</p>
+        </div>
+      </div>
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text-text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Hendra Agil</p>
+          <p className="grey-text">28 August, 4pm</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectList;
