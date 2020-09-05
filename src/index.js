fc8655bb1f7e4cb028e9e56b94a89e3428324bcb
hendra-agil <agilsyaputra26@gmail.com>
2020-09-05 18:44:15 +0700

    Connecting Redux to Firebase

--- src/index.js
+++ src/index.js
@@ -3,16 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './store/reducers/rootReducer';
 import thunk from 'redux-thunk';
+import {
+  reduxFirestore,
+  getFirestore,
+  createFirestoreInstance,
+} from 'redux-firestore';
+import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase';
+import fbConfig from './config/fbConfig';
+import firebase from 'firebase/app';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })),
+    reduxFirestore(fbConfig)
+  )
+);
+
+const rrfProps = {
+  firebase,
+  config: fbConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance,
+};
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
   </Provider>,
   document.getElementById('root')
 );
--- src/store/actions/projectAction.js
+++ src/store/actions/projectAction.js
@@ -1,5 +1,5 @@
 export const createProject = (project) => {
-  return (dispatch, getState) => {
+  return (dispatch, getState, { getFirestore, getFirebase }) => {
     // Make async call to database
     dispatch({ type: 'CREATE_PROJECT', project });
   };
