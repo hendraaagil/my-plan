be8ab3f79bca5a64a69a06d6595b3f2e563ad9ce
hendra-agil <agilsyaputra26@gmail.com>
2020-09-09 14:29:58 +0700

    Firebase auth with redux

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
+import { connect } from 'react-redux';
 
 const Navbar = () => {
   return (
@@ -17,4 +18,9 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+const mapStateToProps = (state) => {
+  console.log(state);
+  return {};
+};
+
+export default connect(mapStateToProps)(Navbar);
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -8,8 +8,8 @@ const ProjectList = ({ projects }) => {
       {projects &&
         projects.map((project) => {
           return (
-            <Link to={`/project/${project.id}`}>
-              <ProjectSummary project={project} key={project.id} />
+            <Link to={`/project/${project.id}`} key={project.id}>
+              <ProjectSummary project={project} />
             </Link>
           );
         })}
