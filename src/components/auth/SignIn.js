e3cff8d6f440507fcb383bcf59569c8875e5a139
hendra-agil <agilsyaputra26@gmail.com>
2020-09-09 14:55:24 +0700

    Logging users in

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { signIn } from '../../store/actions/authAction';
 
 class SignIn extends Component {
   state = {
@@ -14,10 +16,12 @@ class SignIn extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
+    this.props.signIn(this.state);
   };
 
   render() {
+    const { authError } = this.props;
+
     return (
       <div className="container">
         <form onSubmit={this.handleSubmit} className="white">
@@ -32,6 +36,9 @@ class SignIn extends Component {
           </div>
           <div className="input-field">
             <button className="btn blue darken-1 z-depth-0">Login</button>
+            <div className="red-text center">
+              {authError ? <p>{authError}</p> : null}
+            </div>
           </div>
         </form>
       </div>
@@ -39,4 +46,16 @@ class SignIn extends Component {
   }
 }
 
-export default SignIn;
+const mapStateToProps = (state) => {
+  return {
+    authError: state.auth.authError,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signIn: (creds) => dispatch(signIn(creds)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,7 +1,24 @@
-const initState = {};
+const initState = {
+  authError: null,
+};
 
 const authReducer = (state = initState, action) => {
-  return state;
+  switch (action.type) {
+    case 'LOGIN_ERROR':
+      console.log('Login error');
+      return {
+        ...state,
+        authError: 'Login failed',
+      };
+    case 'LOGIN_SUCCESS':
+      console.log('Login success');
+      return {
+        ...state,
+        authError: null,
+      };
+    default:
+      return state;
+  }
 };
 
 export default authReducer;
