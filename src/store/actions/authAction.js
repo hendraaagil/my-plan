148c1f8c459c6f418802163e1c37082b17d0358b
hendra-agil <agilsyaputra26@gmail.com>
2020-09-12 21:02:52 +0700

    User signup

--- src/store/actions/authAction.js
+++ src/store/actions/authAction.js
@@ -26,3 +26,30 @@ export const signOut = () => {
       });
   };
 };
+
+export const signUp = (newUser) => {
+  return (dispatch, getState, { getFirestore, getFirebase }) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(newUser.email, newUser.password)
+      .then((res) => {
+        return firestore
+          .collection('users')
+          .doc(res.user.uid)
+          .set({
+            firstName: newUser.firstName,
+            lastName: newUser.lastName,
+            initials: newUser.firstName[0] + newUser.lastName[0],
+          });
+      })
+      .then(() => {
+        dispatch({ type: 'SIGNUP_SUCCESS' });
+      })
+      .catch((err) => {
+        dispatch({ type: 'SIGNUP_ERROR', err });
+      });
+  };
+};
