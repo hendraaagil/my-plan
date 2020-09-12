6cd8a4f67b265b291b27d20cdd0402ddac8a1b1f
hendra-agil <agilsyaputra26@gmail.com>
2020-09-12 20:10:07 +0700

    Logging users out

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signOut } from '../../store/actions/authAction';
 
-const SignedInLinks = () => {
+const SignedInLinks = (props) => {
   return (
     <ul className="right">
       <li>
         <NavLink to="/create">New Project</NavLink>
       </li>
       <li>
-        <NavLink to="/">Log Out</NavLink>
+        <a onClick={props.signOut}>Log Out</a>
       </li>
       <li>
         <NavLink to="/" className="btn btn-floating blue darken-1">
@@ -19,4 +21,10 @@ const SignedInLinks = () => {
   );
 };
 
-export default SignedInLinks;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signOut: () => dispatch(signOut()),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(SignedInLinks);
--- src/store/actions/authAction.js
+++ src/store/actions/authAction.js
@@ -13,3 +13,16 @@ export const signIn = (credentials) => {
       });
   };
 };
+
+export const signOut = () => {
+  return (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        dispatch({ type: 'SIGNOUT_SUCCESS' });
+      });
+  };
+};
