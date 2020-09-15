29a060670c5aa2534f646f3c7a036667a1a4ad8a
hendra-agil <agilsyaputra26@gmail.com>
2020-09-16 00:13:01 +0700

    Showing notifications

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -8,7 +8,7 @@ import { Redirect } from 'react-router-dom';
 
 class Dashboard extends Component {
   render() {
-    const { projects, auth } = this.props;
+    const { projects, auth, notifications } = this.props;
     if (!auth.uid) return <Redirect to="/signin" />;
 
     return (
@@ -18,7 +18,7 @@ class Dashboard extends Component {
             <ProjectList projects={projects} />
           </div>
           <div className="col s12 m5 offset-m1">
-            <Notifications />
+            <Notifications notifications={notifications} />
           </div>
         </div>
       </div>
@@ -31,10 +31,14 @@ const mapStateToProps = (state) => {
   return {
     projects: state.firestore.ordered.projects,
     auth: state.firebase.auth,
+    notifications: state.firestore.ordered.notifications,
   };
 };
 
 export default compose(
   connect(mapStateToProps),
-  firestoreConnect([{ collection: 'projects' }])
+  firestoreConnect([
+    { collection: 'projects' },
+    { collection: 'notifications', limit: 3 },
+  ])
 )(Dashboard);
--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import moment from 'moment';
 
 const Notifications = (props) => {
+  const { notifications } = props;
+
   return (
     <div className="section">
       <div className="card z-depth-0">
         <div className="card-content">
           <span className="card-title">Notifications</span>
           <ul className="notifications">
-            <li>Notification</li>
-            <li>Notification</li>
-            <li>Notification</li>
-            <li>Notification</li>
+            {notifications &&
+              notifications.map((notification) => {
+                return (
+                  <li key={notification.id}>
+                    <span className="blue-text text-darken-2">
+                      {notification.user}
+                    </span>
+                    <p>{notification.content}</p>
+                    <div className="grey-text note-date">
+                      {moment(notification.time.toDate()).fromNow()}
+                    </div>
+                  </li>
+                );
+              })}
           </ul>
         </div>
       </div>
